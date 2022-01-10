14a9347afba64def29ed3eebf727cb3900a240d4
whdrnal <whdrnal34@gmail.com>
2022-01-10 20:49:52 +0900

    asd

--- base/settings/prod.py
+++ base/settings/prod.py
@@ -8,7 +8,7 @@ DATABASES = {
         'NAME': 'site7_dev',  # db에 데이터베이스가 생성되어있는지 먼저 체크
         'USER': 'whdrnal',  # 유저가 제대로 추가되어있는지 먼저 체크
         'PASSWORD': 'dudwls85',
-        'HOST': '127.0.0.1',
+        'HOST': '172.17.0.1',
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",  # 추가, 만약에 이 부분 때문에 오류가 난다면, 이 라인을 지우고 다시 시도해주세요.
--- base/urls.py
+++ base/urls.py
@@ -13,11 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.http import HttpResponse
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('', lambda request:HttpResponse("Updated!")),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    # DDT 를 위한 설정
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
